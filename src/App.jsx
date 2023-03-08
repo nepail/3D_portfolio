8720b31f99c845c9571088c10502ad8f3740b64c
nepail <kls18z@hotmail.com>
2023-03-08 16:55:05 +0800

    update package

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
+import { BrowserRouter } from "react-router-dom";
+
+import {
+  About,
+  Contact,
+  Experience,
+  Feedbacks,
+  Hero,
+  Navbar,
+  Tech,
+  Works,
+  StarsCanvas,
+} from "./components";
+
 const App = () => {
+  return <div>3D Developer Portfolio</div>;
+};
 
-  return (
-    <div>
-      3D Developer Portfolio
-    </div>
-  )
-}
-
-export default App
+export default App;
