e7d5f8a5042b97301c289c14138114c5481c7040
nepail <kls18z@hotmail.com>
2023-03-09 17:13:13 +0800

    update section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import { motion } from "framer-motion";
+
+import { styles } from "../styles";
+import { ComputersCanvas } from "./canvas";
 
 const Hero = () => {
   return (
-    <div>Hero</div>
-  )
-}
+    <section className="relative w-full h-screen mx-auto">
 
-export default Hero
+    </section>
+  )
+};
+
+export default Hero;
