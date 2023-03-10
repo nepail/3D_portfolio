caebdb90e11dcd8cc9747a8edfb6dcfdf96d55b6
nepail <kls18z@hotmail.com>
2023-03-10 17:01:26 +0800

    add spotlight

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -8,9 +8,16 @@ const Computers = () => {
 
   return (
     <mesh>
-      <hemisphereLight intensity={0.15} 
-      groundColor="black" />
+      <hemisphereLight intensity={0.15} groundColor="black" />
       <pointLight intensity={1} />
+      <spotLight 
+        position={[-20, 50, 10]}
+        angle={0.12}  
+        penumbra={1}
+        intensity={1}
+        castShadow
+        shadow-mapSize={1024}
+      />
       <primitive 
         object={computer.scene} 
         scale={0.75}
