be61ac99e981fed3fda8aa81c30366926c2841ba
nepail <kls18z@hotmail.com>
2023-03-10 16:19:05 +0800

    add despriction

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,6 +11,12 @@ const Hero = () => {
           <div className="w-5 h-5 rounded-full bg-[#915eff]"/>
           <div className="w-1 sm:h-80 h-40 violet-gradient"/>
         </div>
+
+        <div>
+          <h1 className={`${styles.heroHeadText} text-white`}>Hi, I'm <span className="text-[#915eff]">Jim</span></h1>
+          <p className={`${styles.heroSubText} mt-2 text-white-100`}>I develop 3D visuals, user <br className="sm:block hidden" />interfaces and web applications.</p>
+        </div>
+
       </div>
     </section>
   )
