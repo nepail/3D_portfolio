fd291bb43c8ad282cf0a73e9d760ea54d5bd1b83
nepail <kls18z@hotmail.com>
2023-03-14 17:41:46 +0800

    add SectionWrapper on Experience.jsx

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -15,15 +15,11 @@ const Experience = () => {
   return (
     <>
       <motion.div variants={textVariant()}>
-        <p className={styles.sectionSubText}>
-          What I have done so far
-        </p>
-        <h2 className={styles.sectionHeadText}>
-          Work Experience.
-        </h2>
+        <p className={styles.sectionSubText}>What I have done so far</p>
+        <h2 className={styles.sectionHeadText}>Work Experience.</h2>
       </motion.div>
     </>
-  )
+  );
 };
 
-export default Experience;
+export default SectionWrapper(Experience, "work");
