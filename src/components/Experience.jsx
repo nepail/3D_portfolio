20b1eb918b07de658894aa96495cc0c4524c6205
nepail <kls18z@hotmail.com>
2023-03-14 17:41:07 +0800

    add des

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -12,7 +12,18 @@ import { SectionWrapper } from "../hoc";
 import { textVariant } from "../utils/motion";
 
 const Experience = () => {
-  return <div>Experience</div>;
+  return (
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>
+          What I have done so far
+        </p>
+        <h2 className={styles.sectionHeadText}>
+          Work Experience.
+        </h2>
+      </motion.div>
+    </>
+  )
 };
 
 export default Experience;
