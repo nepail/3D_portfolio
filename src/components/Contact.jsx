c1d6bca46445794e54eadfafa347dcc5a7f8ef8f
nepail <kls18z@hotmail.com>
2023-03-15 12:23:45 +0800

    add Contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import { useState, useRef } from "react";
+import { motion } from "framer-motion";
+import emailjs from "@emailjs/browser";
+
+import { styles } from "../styles";
+import { EarthCanvas } from "./canvas";
+import { SectionWrapper } from "../hoc";
+import { slideIn } from "../utils/motion";
 
 const Contact = () => {
-  return (
-    <div>Contact</div>
-  )
-}
+  const formRef = useRef()
+  const [form, setForm] = useState({
+    name: '',
+    email: '',
+    message: ''
+  })
 
-export default Contact
+  const [loading, setLoading] = useState(false)
+
+  const handleChange = (e) => {}
+
+  const handleSubmit = (e) => {}
+
+  return <div>Contact</div>;
+};
+
+export default Contact;
